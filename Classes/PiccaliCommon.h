0f2a984579c9604c4a7f5c9d149bbe65a2c7c89d
ryu22e <ryu22e@gmail.com>
2011-03-15 21:06:06 +0900

    定数を別ファイルに分けた。

---
+++ Classes/PiccaliCommon.h
@@ -0,0 +1,16 @@
+//
+//  PiccaliCommon.h
+//  Piccali
+//
+//  Created by 筒井 隆次 on 11/03/15.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+#define USERDEFAULTS_TWITPIC_ENABLE @"twitpic_enable"
+#define USERDEFAULTS_WASSR_ENABLE @"wassr_enable"
+#define USERDEFAULTS_TWITPIC_USERNAME @"twitpic_username"
+#define USERDEFAULTS_WASSR_USERNAME @"wassr_username"
+#define USERDEFAULTS_TWITPIC_PASSWORD @"twitpic_password"
+#define USERDEFAULTS_WASSR_PASSWORD @"wassr_password"
+#define MAX_LENGTH_TWITPIC 140
+#define MAX_LENGTH_WASSR 255
+#define MAX_LENGTH_OTHER 0
