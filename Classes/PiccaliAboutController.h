66620c7aa8de5f3cd2f43e5ed0c2ba4d32f44896
ryu22e <ryu22e@gmail.com>
2011-04-12 17:56:29 +0900

    アプリケーションの名前をplistから取得するようにした。

--- Classes/PiccaliAboutController.h
+++ Classes/PiccaliAboutController.h
@@ -10,10 +10,10 @@
 #import "PiccaliCommon.h"
 
 #define RYU22E_URL @"https://twitter.com/#!/ryu22e"
-#define TITLE @"About Piccali"
 
 @interface PiccaliAboutController : UIViewController {
 @private
+    IBOutlet UILabel *appName;
     IBOutlet UILabel *versionNumber;
 }
 @property (nonatomic, retain) UILabel *versionNumber;
